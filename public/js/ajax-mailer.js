965ff6f872bbdf294dc45676dedee7b17f74e22f
vzxxl <vzxxl@yandex.ru>
2018-03-01 21:15:18 +0300

    fixed errors

--- public/js/ajax-mailer.js
+++ public/js/ajax-mailer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function() {
 
 //Cashing the form
@@ -25,32 +24,4 @@ $('.form-style-4').fadeOut(100000);
   console.log('fail');
 })
 
-=======
-$(document).ready(function() {
-
-//Cashing the form
-var form = $('.form-style-4');
-
-//Prevent form from refreshing the page
-$(form).click(function(e) {
-	e.preventDefault();
-});
-
-//Getting the data from the form in the proper condition for mail.php
-var formData = $(form).serialize();
-
-//Transfer the form using AJAX
-$.ajax({
-	type: 'POST',
-	url: $(form).attr('action'),
-	data: formData
-})
-.done(function(response) {
-$('.form-style-4').fadeOut(100000);
-})
-.fail(function(data) {
-  console.log('fail');
-})
-
->>>>>>> a9478deeaa78fc58070394eb466903ae7262264c
 });
--- public/js/swiper-custom.js
+++ public/js/swiper-custom.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Swiper
 
 
@@ -20,27 +19,4 @@
             modifier: 1,
             slideShadows : true
         }
-=======
-//Swiper
-
-
-    var mySwiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        effect: 'coverflow',
-        grabCursor: true,
-        //Observer
-        //observer: true,
-        //observeParents: true,
-        
-        centeredSlides: true,
-        keyboardControl: true,
-        slidesPerView: 'auto',
-        coverflow: {
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows : true
-        }
->>>>>>> a9478deeaa78fc58070394eb466903ae7262264c
     });
