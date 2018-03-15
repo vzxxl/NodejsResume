12ed87dc6f26a6c42398fee5339dabd17af340d8
vzxxl <vzxxl@yandex.ru>
2018-03-15 17:44:06 +0300

    minor

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     $generalContainer.toggleClass('low-index');
     $swiperContainer.toggleClass('low-index');
     $('.navbar-collapse').toggleClass('blackbcg');
-    $('.nav-collapsed').toggleClass('collapse in');
+    $('.nav-collapsed').toggleClass('in');
   });
 
 
